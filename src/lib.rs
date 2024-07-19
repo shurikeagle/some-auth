73daf0d5aca14007a7966843d2b70108dda94fcd
A.Orlov <A.Orlov@proresearch.am>
2024-07-19 15:11:29 +0400

    add necessary public usings

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,6 @@ mod jwt;
 mod hasher;
 
 pub use error::AuthError;
-pub use user_service::{ UserService, UserServiceBuilder, builder, default_builder, User};
+pub use user_service::{ AuthUser, UserService, UserServiceBuilder, builder, default_builder, User};
+pub use jwt::{ JwtTokenSettings, TokenPair };
+pub use repository::AuthRepository;
