57a9aa755c7165dc0a19ca8837952c7a728ad68d
A.Orlov <A.Orlov@proresearch.am>
2024-07-25 17:28:29 +0400

    add admin check into middleware

--- src/axum_middleware.rs
+++ src/axum_middleware.rs
@@ -40,13 +40,14 @@ pub struct  UserServiceState<TAuthUser: AuthUser + fmt::Debug + Send + Sync> {
 pub async fn auth_middleware<TAuthUser: AuthUser + fmt::Debug + Send + Sync>(
     State(state): State<Arc<UserServiceState<TAuthUser>>>,
     req: Request,
-    next: Next
+    next: Next,
+    admin_only: bool
 ) -> Result<Response, AuthError> {
     let auth_header = req.headers().get(http::header::AUTHORIZATION).ok_or(AuthError::Unathorized)?;
     let access_token = auth_header.to_str().map_err(|_| AuthError::Internal("Couldn't handle user token".to_string()))?;
 
     let _ = state.user_service
-        .get_authenticated_user(access_token, false)
+        .get_authenticated_user(access_token, admin_only)
         .await
         .map_err(|err| err)?;
 
