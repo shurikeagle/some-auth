c6fc11340528272dd648ea280e2bcad564f86f0a
A.Orlov <A.Orlov@proresearch.am>
2024-07-25 16:39:35 +0400

    change error responses

--- src/axum_middleware.rs
+++ src/axum_middleware.rs
@@ -1,8 +1,35 @@
 use std::{fmt, sync::Arc};
 
-use axum::{extract::{Request, State}, http::{self}, middleware::Next, response::Response };
+use axum::{extract::{Request, State}, http::{self, StatusCode}, middleware::Next, response::{IntoResponse, Response}, Json };
+use serde::Serialize;
 
 use crate::{AuthError, AuthUser, UserService};
+
+#[derive(Serialize)]
+pub(super) struct AuthErrorResponse {
+    /// Error message
+    pub message: String
+}
+
+impl IntoResponse for AuthError {
+    fn into_response(self) -> Response {
+        let (code, message) = match self {
+            AuthError::Internal(msg) => (StatusCode::INTERNAL_SERVER_ERROR, msg),
+            AuthError::ValidationError(msg) | AuthError::InvalidOperation(msg) => (StatusCode::BAD_REQUEST, msg),
+            AuthError::UsernameUnavailable => (StatusCode::BAD_REQUEST, self.to_string()),
+            AuthError::InvalidCredentials => (StatusCode::FORBIDDEN, self.to_string()),
+            AuthError::AuthRepositoryError(_) => (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()),
+            AuthError::UserNotFound(_) => (StatusCode::NOT_FOUND, self.to_string()),
+            AuthError::Unathorized => (StatusCode::UNAUTHORIZED, self.to_string())
+        };
+
+        let response = Json(AuthErrorResponse {
+            message
+        });
+
+        (code, response).into_response()
+    }
+}
 
 /// Router [`State`] which must be used for [`auth_middleware`]
 pub struct  UserServiceState<TAuthUser: AuthUser + fmt::Debug + Send + Sync> {
