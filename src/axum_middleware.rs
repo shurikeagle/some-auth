bff9c5d5858e3de5149a6a4ecf87fff5d538ac3e
A.Orlov <A.Orlov@proresearch.am>
2024-07-25 16:13:48 +0400

    change UserServiceState from trait to struct

--- src/axum_middleware.rs
+++ src/axum_middleware.rs
@@ -1,24 +1,24 @@
 use std::{fmt, sync::Arc};
 
-use axum::{body::Body, extract::{Request, State}, http::{self}, middleware::Next, response::Response };
+use axum::{extract::{Request, State}, http::{self}, middleware::Next, response::Response };
 
 use crate::{AuthError, AuthUser, UserService};
 
-/// Router [`State`] trait which must be used for [`auth_middleware`]
-pub trait UserServiceState<TAuthUser: AuthUser + fmt::Debug + Send + Sync> {
-    fn user_service(&self) -> Arc<UserService<TAuthUser>>;
+/// Router [`State`] which must be used for [`auth_middleware`]
+pub struct  UserServiceState<TAuthUser: AuthUser + fmt::Debug + Send + Sync> {
+    user_service: Arc<UserService<TAuthUser>>
 }
 
 /// Controls if user is authenticated and optionally checks if user is admin
 pub async fn auth_middleware<TAuthUser: AuthUser + fmt::Debug + Send + Sync>(
-    State(state): State<Arc<dyn UserServiceState<TAuthUser>>>,
-    req: Request<Body>,
+    State(state): State<UserServiceState<TAuthUser>>,
+    req: Request,
     next: Next
 ) -> Result<Response, AuthError> {
     let auth_header = req.headers().get(http::header::AUTHORIZATION).ok_or(AuthError::Unathorized)?;
     let access_token = auth_header.to_str().map_err(|_| AuthError::Internal("Couldn't handle user token".to_string()))?;
 
-    let _ = state.user_service()
+    let _ = state.user_service
         .get_authenticated_user(access_token, false)
         .await
         .map_err(|err| err)?;
